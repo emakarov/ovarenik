e8fdb1e74a3b3ae6c9041e634d5c7ecc59ec336b
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-08-10 11:48:33 +0400

    launcher redactor

--- ovarenik/static/redactor/redactorlauncher.js
+++ ovarenik/static/redactor/redactorlauncher.js
@@ -1,8 +1,8 @@
 Suit.$(function(){
   Suit.$('textarea').redactor({
     focus: true,
-    imageUpload: '/ru/blog/uploadimagejson/',
+    imageUpload: '/blog/uploadimagejson/',
     fileUpload: '../demo/scripts/file_upload.php',
-    imageGetJson: '/ru/blog/redactorimagejson/'
+    imageGetJson: '/blog/redactorimagejson/'
   });
 });
