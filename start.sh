f56cf7fca8209305010ffe3de7f78fae659b2f78
Evgeni <makarov@dpsecommerce.(none)>
2013-08-11 02:46:44 +0400

    shell script modified

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run this as the djangorunner user!
 
-VIRTUALENV_DIR=/home/makarov/env/ovarenik
+VIRTUALENV_DIR=/home/makarov/envs/ovarenik
 source $VIRTUALENV_DIR/bin/activate
 cd /home/makarov/ovarenik/
 python manage.py runfcgi method=threaded debug=False host=127.0.0.1 port=8099 pidfile=/home/makarov/runfiles/ovarenik.pid
